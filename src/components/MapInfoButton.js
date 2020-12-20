143f45c687d784592f75db401376ba88801667ea
N-Hoque <Naimul_Hoque@outlook.com>
2020-12-20 21:19:35 +0000

    Added MapInfoButton and MapInfoDisplay

---
+++ src/components/MapInfoButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MapInfoButton.css";
+
+class MapInfoButton extends React.Component {
+  render() {
+    return (
+      <div className="map-info-button">
+        <button>
+          Hyderabad
+        </button>
+      </div>
+    );
+  }
+}
