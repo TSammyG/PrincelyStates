8542408d55bfdd90971d709c973f69c924a6ea42
TSammyG <s.goodland@hotmail.co.uk>
2020-12-24 16:02:48 +0000

    Learning how to use React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as ReactBootStrap from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const states = [
+    { state: "Hyderabad", ruler: "Osman Ali Khan", year: "1936" },
+    { state: "Mysore", ruler: "Krishnaraja Wadiyar IV", year: "1936" },
+    { state: "Bikaner", ruler: "Ganga Singh", year: "1936" },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactBootStrap.Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>State</th>
+            <th>Ruling Prince</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>{states.map(renderState)}</tbody>
+      </ReactBootStrap.Table>
     </div>
   );
-}
+};
+
+const renderState = (state, index) => {
+  return (
+    <tr key={index}>
+      <td>{state.state}</td>
+      <td>{state.ruler}</td>
+      <td>{state.year}</td>
+    </tr>
+  );
+};
 
 export default App;
